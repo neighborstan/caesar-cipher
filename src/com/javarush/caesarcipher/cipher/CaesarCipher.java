96c6ec48bda4583134cafbcbe8d1b3dfc77163fa
neighborstan <dropweb.post@gmail.com>
2022-03-19 15:04:28 +0300

    Revert "Refactoring performed"
    This reverts commit a40f7d0016dbeef8d9d3ae28bff8bca8a55acc7c.

--- src/com/javarush/caesarcipher/cipher/CaesarCipher.java
+++ src/com/javarush/caesarcipher/cipher/CaesarCipher.java
@@ -7,34 +7,33 @@ import java.util.List;
 public class CaesarCipher {
     private static CaesarCipher instance;
 
-    private static final List<Character> ALPHABET = Arrays.asList(
+    private static final List<Character> ALPHABET
+            = Arrays.asList(
             'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
             'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
             'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
             'э', 'ю', 'я', ' ', '!', '.', ',', '"', '?', ':',
-            '\\', '\''
-    );
+            '\\', '\'');
 
-    private static final List<String> KEYWORDS = Arrays.asList(
+    private static final List<String> KEYWORDS
+            = Arrays.asList(
             " не ", " я ", " и ", " на ", " с ",
             " в ", " к ", " под ", " из ", ", ",
-            ": ", ". ", "! ", "? ", " - ", " у "
-    );
+            ": ", ". ", "! ", "? ", " - ", " у ");
 
-    private CaesarCipher() {
-    }
+    private CaesarCipher() {}
 
-    public static CaesarCipher getInstance() {
-        if (instance == null) {
+    public static CaesarCipher getInstance(){
+        if(instance == null){
             instance = new CaesarCipher();
         }
         return instance;
     }
 
-    public void encrypt(Path inputFilePath, Path outputFilePath, int cipherKey) {
+    public void encrypt(Path pathToDecryptedFile, Path pathToEncryptedFile, int cipherKey) {
         StringBuilder builderResultText = new StringBuilder();
 
-        String decryptedText = FileProcessing.readString(inputFilePath);
+        String decryptedText = FileProcessing.readString(pathToDecryptedFile);
 
         for (char symbol : decryptedText.toCharArray()) {
             if (ALPHABET.contains(Character.toLowerCase(symbol))) {
@@ -43,15 +42,15 @@ public class CaesarCipher {
                 int encryptSymbolIndex = (alphabetSymbolIndex + cipherKey) % alphabetSize();
                 symbol = getAlphabetSymbol(encryptSymbolIndex);
             }
-            builderResultText.append(symbol);
+                builderResultText.append(symbol);
         }
-        FileProcessing.writeString(outputFilePath, builderResultText.toString());
+        FileProcessing.writeString(pathToEncryptedFile, builderResultText.toString());
     }
 
-    public void decrypt(Path inputFilePath, Path outputFilePath, int cipherKey) {
+    public void decrypt(Path pathToEncryptedFile, Path pathToDecryptedFile, int cipherKey) {
         StringBuilder builderResultText = new StringBuilder();
 
-        String encryptedText = FileProcessing.readString(inputFilePath);
+        String encryptedText = FileProcessing.readString(pathToEncryptedFile);
 
         for (char symbol : encryptedText.toCharArray()) {
             if (ALPHABET.contains(Character.toLowerCase(symbol))) {
@@ -60,60 +59,47 @@ public class CaesarCipher {
                 int decryptSymbolIndex = (alphabetSymbolIndex + (alphabetSize() - cipherKey)) % alphabetSize();
                 symbol = getAlphabetSymbol(decryptSymbolIndex);
             }
-            builderResultText.append(symbol);
+                builderResultText.append(symbol);
         }
-        FileProcessing.writeString(outputFilePath, builderResultText.toString());
+        FileProcessing.writeString(pathToDecryptedFile, builderResultText.toString());
     }
 
-    public void bruteForce(Path inputFilePath, Path outputFilePath, int textSizeKey) {
+    public void bruteForce(Path pathToEncryptedFile, Path pathToDecryptedFile, int bruteForceKey) {
         StringBuilder builderResultText = new StringBuilder();
 
-        String encryptedText = FileProcessing.readString(inputFilePath);
-        int cipherKey = 1;
+            String encryptedText = FileProcessing.readString(pathToEncryptedFile);
+            int cipherKey = 1;
 
-        while (cipherKey < alphabetSize()) {
+            while (cipherKey < alphabetSize()) {
 
-            for (char symbol : encryptedText.toCharArray()) {
-                if (ALPHABET.contains(Character.toLowerCase(symbol))) {
+                for (char symbol : encryptedText.toCharArray()) {
+                    if (ALPHABET.contains(Character.toLowerCase(symbol))) {
 
-                    int alphabetSymbolIndex = getAlphabetSymbolIndex(symbol);
-                    int decryptSymbolIndex = (alphabetSymbolIndex + cipherKey) % alphabetSize();
-                    symbol = getAlphabetSymbol(decryptSymbolIndex);
+                        int alphabetSymbolIndex = getAlphabetSymbolIndex(symbol);
+                        int decryptSymbolIndex = (alphabetSymbolIndex + cipherKey) % alphabetSize();
+                        symbol = getAlphabetSymbol(decryptSymbolIndex);
+                    }
+                        builderResultText.append(symbol);
                 }
-                builderResultText.append(symbol);
+
+                String resultString = builderResultText.toString();
+                int keywordHitsCount = 0;
+
+                for (String keyword : KEYWORDS) {
+                    if (resultString.contains(keyword)) keywordHitsCount++;
+                }
+
+                if (keywordHitsCount >= bruteForceKey && (resultString.endsWith(".") || resultString.endsWith("!") || resultString.endsWith("?"))
+                ) {
+                    System.out.println("Ключ шифрования: " + (alphabetSize() - cipherKey));
+                    FileProcessing.writeString(pathToDecryptedFile, resultString);
+                    return;
+                } else {
+                    builderResultText.setLength(0);
+                    cipherKey++;
+                }
             }
-            String result = builderResultText.toString();
-            int keywordHitsCount = getKeywordHitsCount(result);
 
-            if (isDecryptedText(keywordHitsCount, textSizeKey, result)) {
-
-                System.out.println("Ключ шифрования: " + (alphabetSize() - cipherKey));
-                FileProcessing.writeString(outputFilePath, result);
-                return;
-            } else {
-                builderResultText.setLength(0);
-                cipherKey++;
-            }
-        }
-    }
-
-    private boolean isDecryptedText(int keywordHitsCount, int textSizeKey, String text){
-        return (keywordHitsCount >= textSizeKey) && isLastSymbolValid(text);
-    }
-
-    private boolean isLastSymbolValid(String text){
-        return text.endsWith(".") || text.endsWith("!") || text.endsWith("?");
-    }
-
-    private int getKeywordHitsCount(String text){
-        int keywordHitsCount = 0;
-
-        for (String keyword : KEYWORDS) {
-            if (text.contains(keyword)) {
-                keywordHitsCount++;
-            }
-        }
-        return keywordHitsCount;
     }
 
     private int getAlphabetSymbolIndex(char symbol) {
--- src/com/javarush/caesarcipher/cipher/FileProcessing.java
+++ src/com/javarush/caesarcipher/cipher/FileProcessing.java
@@ -5,17 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileProcessing {
-    private static final String READ_FILE_ERROR = "Ошибка при чтении файла!";
-    private static final String WRITE_FILE_ERROR = "Ошибка при записи файла!";
 
     public static String readString(Path pathToFile){
-        String resultText;
+        String resultText = "";
         try {
             resultText = Files.readString(pathToFile);
 
         } catch (IOException e) {
-            System.err.println(READ_FILE_ERROR);
-            resultText = e.getMessage();
             e.printStackTrace();
         }
         return resultText;
@@ -25,7 +21,6 @@ public class FileProcessing {
         try {
             Files.writeString(pathToFile, textString);
         } catch (IOException e) {
-            System.err.println(WRITE_FILE_ERROR);
             e.printStackTrace();
         }
     }
