53c2e8147acfae9ed4790cd6bf18c8a6ba13b777
neighborstan <dropweb.post@gmail.com>
2022-03-19 22:46:37 +0300

    Refactoring performed ===
    UserDialog
    - part of the code is placed in separate method: isValidCipherKeyRange
    - changed getBruteForceKey method, switch removed, replaced by enum methods
    - replaced switch by if/else in start()

--- src/com/javarush/caesarcipher/dialog/UserDialog.java
+++ src/com/javarush/caesarcipher/dialog/UserDialog.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import static com.javarush.caesarcipher.dialog.ConsoleText.*;
 
 public class UserDialog {
-    private final String BRUTE_FORCE_KEYS_LIST = "%s - небольшой текст\n%s - средний текст\n%s - большой текст".formatted(SMALL_TEXT_KEY, MEDIUM_TEXT_KEY, LARGE_TEXT_KEY);
+    private final String BRUTE_FORCE_KEYS_LIST = "%s - небольшой текст\n%s - средний текст\n%s - большой текст".formatted(TextSizeKey.SMALL.getStringKey(), TextSizeKey.MEDIUM.getStringKey(), TextSizeKey.LARGE.getStringKey());
     private final String ACTIONS_LIST_TEXT = "%s - зашифровать файл\n%s - расшифровать файл по ключу\n%s - расшифровать файл подбором (brute force)".formatted(ENCRYPT_ACTION_KEY, DECRYPT_ACTION_KEY, BRUTE_FORCE_ACTION_KEY);
 
     private final CaesarCipher CAESAR_CIPHER = CaesarCipher.getInstance();
@@ -19,12 +19,11 @@ public class UserDialog {
     private Path pathToDecryptedFile;
     private Path pathToEncryptedFile;
     private int cipherKey;
-    private int bruteForceKey;
+    private int bruteForceTextKey;
 
 
     public void start() {
         System.out.println(HELLO_TEXT);
-
 
         try (Scanner scanner = new Scanner(System.in)) {
             String action;
@@ -35,98 +34,82 @@ public class UserDialog {
                 action = scanner.nextLine();
             } while (!isActionValid(action));
 
-            switch (action) {
-                case ENCRYPT_ACTION_KEY -> {
+            if (ENCRYPT_ACTION_KEY.equals(action)) {
+                System.out.println(ENCRYPTION_SELECTED_MESSAGE);
+                System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
+                pathToDecryptedFile = getPathToFile(scanner);
 
-                    System.out.println(ENCRYPTION_SELECTED_MESSAGE);
-                    System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
-                    pathToDecryptedFile = getPathToFile(scanner);
+                System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
+                pathToEncryptedFile = getPathToFile(scanner);
 
-                    System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
-                    pathToEncryptedFile = getPathToFile(scanner);
+                cipherKey = getValidCipherKey(scanner);
 
-                    cipherKey = getValidCipherKey(scanner);
+                CAESAR_CIPHER.encrypt(pathToDecryptedFile, pathToEncryptedFile, cipherKey);
 
-                    CAESAR_CIPHER.encrypt(pathToDecryptedFile, pathToEncryptedFile, cipherKey);
+                System.out.println(RESULT_ACTION_MESSAGE + pathToEncryptedFile.toAbsolutePath());
 
-                    System.out.println(RESULT_ACTION_MESSAGE + pathToEncryptedFile.toAbsolutePath());
-                }
-                case DECRYPT_ACTION_KEY -> {
+            } else if (DECRYPT_ACTION_KEY.equals(action)) {
+                System.out.println(DECRYPTION_SELECTED_MESSAGE);
+                System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
+                pathToEncryptedFile = getPathToFile(scanner);
 
-                    System.out.println(DECRYPTION_SELECTED_MESSAGE);
-                    System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
-                    pathToEncryptedFile = getPathToFile(scanner);
+                System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
+                pathToDecryptedFile = getPathToFile(scanner);
 
-                    System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
-                    pathToDecryptedFile = getPathToFile(scanner);
+                cipherKey = getValidCipherKey(scanner);
 
-                    cipherKey = getValidCipherKey(scanner);
+                CAESAR_CIPHER.decrypt(pathToEncryptedFile, pathToDecryptedFile, cipherKey);
 
-                    CAESAR_CIPHER.decrypt(pathToEncryptedFile, pathToDecryptedFile, cipherKey);
+                System.out.println(RESULT_ACTION_MESSAGE + pathToDecryptedFile.toAbsolutePath());
 
-                    System.out.println(ConsoleText.RESULT_ACTION_MESSAGE + pathToDecryptedFile.toAbsolutePath());
-                }
-                case BRUTE_FORCE_ACTION_KEY -> {
+            } else if (BRUTE_FORCE_ACTION_KEY.equals(action)) {
+                System.out.println(BRUTE_FORCE_SELECTED_MESSAGE);
+                System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
+                pathToEncryptedFile = getPathToFile(scanner);
 
-                    System.out.println(BRUTE_FORCE_SELECTED_MESSAGE);
-                    System.out.println(ENTER_PATH_TO_ENCRYPTED_FILE_MESSAGE);
-                    pathToEncryptedFile = getPathToFile(scanner);
+                System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
+                pathToDecryptedFile = getPathToFile(scanner);
 
-                    System.out.println(ENTER_PATH_TO_DECRYPTED_FILE_MESSAGE);
-                    pathToDecryptedFile = getPathToFile(scanner);
+                bruteForceTextKey = getBruteForceTextKey(scanner);
 
-                    bruteForceKey = getBruteForceKey(scanner);
+                CAESAR_CIPHER.bruteForce(pathToEncryptedFile, pathToDecryptedFile, bruteForceTextKey);
 
-                    CAESAR_CIPHER.bruteForce(pathToEncryptedFile, pathToDecryptedFile, bruteForceKey);
-
-                    System.out.println(ConsoleText.RESULT_ACTION_MESSAGE + pathToDecryptedFile.toAbsolutePath());
-                }
+                System.out.println(RESULT_ACTION_MESSAGE + pathToDecryptedFile.toAbsolutePath());
             }
         }
     }
 
     private boolean isActionValid(String action) {
-        return !ENCRYPT_ACTION_KEY.equals(action) && !DECRYPT_ACTION_KEY.equals(action) && !BRUTE_FORCE_ACTION_KEY.equals(action);
+        return ENCRYPT_ACTION_KEY.equals(action) || DECRYPT_ACTION_KEY.equals(action) || BRUTE_FORCE_ACTION_KEY.equals(action);
     }
 
     private int getValidCipherKey(Scanner scanner) {
-        int key;
-        String keyString;
-
         while (true) {
             System.out.println(ENTER_CIPHER_KEY_MESSAGE + (ALPHABET_SIZE - 1));
 
-            keyString = scanner.nextLine();
+            String keyString = scanner.nextLine();
             if (isInteger(keyString)) {
-                key = Integer.parseInt(keyString);
+                int key = Integer.parseInt(keyString);
 
-                if (key > 0 && key < ALPHABET_SIZE) {
+                if (isValidCipherKeyRange(key)) {
                     return key;
                 }
             }
         }
     }
 
-    private int getBruteForceKey(Scanner scanner) {
-        int bruteForceKey;
+    private int getBruteForceTextKey(Scanner scanner) {
         String stringKey;
+        System.out.println(CHOISE_OF_BRUTE_FORCE_KEY_MESSAGE);
+        System.out.println(BRUTE_FORCE_KEYS_LIST);
 
-        while (true) {
+        while (!TextSizeKey.getKeys().containsKey(stringKey = scanner.nextLine())) {
             System.out.println(CHOISE_OF_BRUTE_FORCE_KEY_MESSAGE);
             System.out.println(BRUTE_FORCE_KEYS_LIST);
-
-            stringKey = scanner.nextLine();
-            switch (stringKey) {
-                case SMALL_TEXT_KEY -> bruteForceKey = 2;
-                case MEDIUM_TEXT_KEY -> bruteForceKey = 5;
-                case LARGE_TEXT_KEY -> bruteForceKey = 8;
-                default -> {
-                    continue;
-                }
-            }
-            return bruteForceKey;
         }
+        return TextSizeKey.getKeys().get(stringKey);
     }
+
 
     private Path getPathToFile(Scanner scanner) {
         Path pathToFile = Path.of(scanner.nextLine());
@@ -136,6 +119,10 @@ public class UserDialog {
             pathToFile = Path.of(scanner.nextLine());
         }
         return pathToFile;
+    }
+
+    private boolean isValidCipherKeyRange(int key){
+        return key > 0 && key < ALPHABET_SIZE;
     }
 
     private boolean isInteger(String keyString) {
